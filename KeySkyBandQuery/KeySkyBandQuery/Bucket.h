c037119e30de6864869a997b2c4486ccac62c769
AlanRuijia <Alanmao94@gmail.com>
2015-07-14 23:14:22 +0800

    Step 3 modified

--- KeySkyBandQuery/KeySkyBandQuery/Bucket.h
+++ KeySkyBandQuery/KeySkyBandQuery/Bucket.h
@@ -14,15 +14,18 @@
 struct gtBucket {
     int bitmap;
     int dataSize;
-		int SlSize;
-		int SlnSize;
+	int SlSize;
+	int SlnSize;
     struct gtPoint *data;
     struct gtPoint *dataTail;
     struct gtPoint *Sl;
     struct gtPoint *SlTail;
     struct gtPoint *Sln;
     struct gtPoint *SlnTail;
-    struct gtPoint *StwhSes;
+    struct gtPoint *Stwh;
+	struct gtPoint *StwhTail;
+	struct gtPoint *Ses;
+	struct gtPoint *SesTail;
     struct gtBucket *next;
     struct gtBucket *previous;
 };
